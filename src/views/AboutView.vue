f8372853d77db33e13d46df8c4dd1b6c6e2cbebc
Adrian Larenaudie <alarenaudie@zaion.ai>
2023-03-23 16:40:12 +0100

    edit text page about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,7 +4,7 @@
         <h1 class="main_content_about_title">Quelques <span :style="{color: getCurrentTheme.word}">mots</span></h1>
         <div class="main_content_about_block_text">
             <p class="main_content_about_paragraph">
-                C'est en 2021 que tout commence, après une période de quelques mois à me former aux téchnologies du web en autodidacte, je décide d'intégrer une école pour me professionnaliser.
+                Courant 2021 après une période de quelques mois à me former aux téchnologies du web en autodidacte, je décide d'intégrer une école pour me professionnaliser.
             </p>
             <p class="main_content_about_paragraph">
                 J'effectue deux formations chez <span :style="{color: getCurrentTheme.word}">O'clock</span>, DWWM puis CDA en alternance. Pour le seconde formation j'ai la chance d'intégrer l'entreprise <span :style="{color: getCurrentTheme.word}">ZAION</span> en tant que développeur backend nodejs.
@@ -14,7 +14,7 @@
                 J'accorde de l'importance à rendre mon code évolutif et à créer des interfaces attractives pour rendre l'expérience utilisateur aussi agréable qu'inoubliable.
             </p>
             <p class="main_content_about_paragraph">
-                Mes expériences m'ont amenées à comprendre et appliquer les concepts inhérents au développement applicatif et mon <span :style="{color: getCurrentTheme.word}">profile</span> s'oriente fullstack javascript.
+                Mes expériences m'ont amenées à comprendre et appliquer les concepts inhérents au développement applicatif ce qui me rend flexible sur la stack technique utilisée tout en ayant un <span :style="{color: getCurrentTheme.word}">profile</span> orienté fullstack javascript.
             </p>
 
         </div>
